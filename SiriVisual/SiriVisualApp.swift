c166a9fc82614dd3cb9e25a2113ddfc2b8cae818
Elaidzha1940 <eli080293@gmail.com>
2023-08-26 12:10:36 +0300

    SiriVisual

---
+++ SiriVisual/SiriVisualApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiriVisualApp.swift
+//  SiriVisual
+//
+//  Created by Elaidzha Shchukin on 26.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SiriVisualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

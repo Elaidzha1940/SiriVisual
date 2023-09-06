ed9e41ac4e193a0b4ea28a74d166794da169d129
Elaidzha1940 <eli080293@gmail.com>
2023-09-06 23:22:20 +0300

    Siri

--- SiriVisual/ContentView.swift
+++ SiriVisual/ContentView.swift
@@ -16,9 +16,24 @@ struct ContentView: View {
     
     var body: some View {
         
-        VStack {
-          
-            
+        ZStack {
+            ZStack {
+                
+                Image("shadow")
+                Image("icon-bg")
+                Image("pink-top")
+                    .rotationEffect(.degrees(isRounding ? 320 : -360))
+                    .hueRotation(.degrees(isRounding ? -270 : 60))
+                
+                Image("pink-left")
+                    .rotationEffect(.degrees(isRounding ? -360 : 180))
+                    .hueRotation(.degrees(isRounding ? -220 : 300))
+
+                Image("middle-blue")
+                    .rotationEffect(.degrees(isRounding ? -360 : 420))
+                    .hueRotation(.degrees(isRounding ? -150 : 0))
+                    .rotation3DEffect(.degrees(75), axis: (x: 1, y: 0, z: isRounding ? -5 : 15))
+            }
         }
     }
 }

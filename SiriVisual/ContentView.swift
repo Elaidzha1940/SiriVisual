d0570456338475f6e433bb5016faf8d4fa9b5788
Elaidzha1940 <eli080293@gmail.com>
2023-09-06 22:42:13 +0300

    Siri

--- SiriVisual/ContentView.swift
+++ SiriVisual/ContentView.swift
@@ -12,27 +12,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    // @State var isRounding = false
-    
-    @State private var isAnimating = false
-    
+    @State var isRounding = false
     
     var body: some View {
         
         VStack {
-            Circle()
-                .frame(width: 100, height: 100)
-                .foregroundColor(.blue)
-                .foregroundColor(.red)
-                .scaleEffect(isAnimating ? 1.3 : 1.0)
-                .opacity(isAnimating ? 0.6 : 1.0)
-                .animation(
-                    Animation.easeInOut(duration: 1)
-                        .repeatForever(autoreverses: true)
-                )
-                .onAppear() {
-                    self.isAnimating = true
-                }
+          
             
         }
     }
@@ -41,18 +26,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-struct Conten: View {
-    var body: some View {
-        VStack {
-            Text("Your App")
-                .foregroundColor(.red)
-                .font(.largeTitle)
-                .padding()
-            
-            ContentView()
-        }
     }
 }
